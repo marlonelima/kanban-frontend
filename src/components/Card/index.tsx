d738999f0f81b1b268b4f64597e145467efb0777
Marlone Lima <marlonelimabr@gmail.com>
2023-03-20 12:41:38 -0300

    Create card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './styles';
+
+function Card() {
+  return (
+    <Container>
+      <Title>card</Title>
+    </Container>
+  );
+}
+
+export default Card;
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 1rem 2rem;
+  background: #fff;
+  border-radius: 0.5rem;
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+  font-size: 0.8rem;
+`;
